2f269103f14c7f7db64702801ed2a975fae0cc41
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-04-06 12:26:29 +0300

    HTML report

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
 
-// Create an object that satisfies the 'DataReader' interface
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Liverpool");
 
-const csvFileReader = new CsvFileReader("football.csv");
-
-// Create an instance of MatchReader and pass is something satisfying the 'DataReader' interface
-
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary (
-    new WinsAnalysis("Man United"),
-    new ConsoleReport()
-);
-
 summary.buildAndPrintReport(matchReader.matches);
