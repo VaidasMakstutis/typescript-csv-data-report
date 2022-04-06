7e63616364a4cba3ed9f0131f815e73bad51de2a
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-04-06 11:32:31 +0300

    Building the reporter

--- build/index.js
+++ build/index.js
@@ -1,20 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const MatchResult_1 = require("./MatchResult");
 const CsvFileReader_1 = require("./CsvFileReader");
+const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+const Summary_1 = require("./Summary");
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
 // Create an instance of MatchReader and pass is something satisfying the 'DataReader' interface
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis("Man United"), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
